b2b360eabffab1d946809bef71d1ed9a44bc19a4
aadhil96 <aadhil.imam@gmail.com>
2018-02-17 09:45:41 -0800

    Encapsulation in java

---
+++ src/Encapsulation.java
@@ -0,0 +1,52 @@
+class EncapTest{
+    private String name;
+    private String idNum;
+    private int age;
+    
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public void setIdNum(String idNum){
+        this.idNum = idNum;
+    }
+    
+    public void setAge(int age){
+        this.age = age;
+    }
+    
+    public String GetName(){
+        return name;
+    }
+    
+    public String GetIdNum(){
+        return idNum;
+    }
+    
+    public int GetAge(){
+        return age;
+    }
+}
+
+
+
+
+
+public class Encapsulation {
+
+    public static void main(String[] args) {
+        EncapTest encap = new EncapTest();
+        encap.setName("Aadhil");
+        encap.setAge(20);
+        encap.setIdNum("120N");
+        
+        
+        System.out.println(encap.GetName());
+        System.out.println(encap.GetIdNum());
+        System.out.println(encap.GetAge());
+        
+        
+    }
+    
+}
